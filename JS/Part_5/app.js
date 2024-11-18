f37852687b77b5dfdc1fae189c72025253be0b30
kasimlohar <kasimlohar2@gmail.com>
2024-11-18 17:55:41 +0530

    JS Part 5

---
+++ JS/Part_5/app.js
@@ -0,0 +1,18 @@
+// let dice = Math.floor(Math.random()*6)+1;
+// console.log(dice);
+
+// const car = {
+//     name: "Maruti",
+//     model: "803",
+//     color: "Red",
+
+// }
+
+// console.log(car.name);
+
+
+// const Person = {
+//     name: "Kasim",
+//     age: 18,
+//     city: "new York",
+// }
